4a08e9989b4336855f7f5d3cead5911e3d3afce4
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-14 17:15:47 +0800

    Add subparsers

--- twilist.py
+++ twilist.py
@@ -3,11 +3,26 @@
 import argparse
 
 class Twilist():
+
     parser = None
     args = None
 
     def __init__(self, *args, **kwargs):
-        self.parser = argparse.ArgumentParser()
+        parser = argparse.ArgumentParser()
+
+        subparsers = parser.add_subparsers()
+
+        add_subparser = subparsers.add_parser('add')
+        add_subparser.add_argument('text')
+
+        send_subparser = subparsers.add_parser('send')
+        send_subparser.add_argument('target', nargs='?',
+                help='first, last, rand, or index from list')
+
+        subparsers.add_parser('list')
+
+        self.parser = parser
+
 
     def process(self):
         self.args = self.parser.parse_args()
