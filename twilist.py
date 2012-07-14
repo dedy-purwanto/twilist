d56d6d3f1a7dcb48944056b7d9141e408b94b7a1
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-14 16:54:27 +0800

    Add basic structure

--- twilist.py
+++ twilist.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 
+import argparse
 
+class Twilist():
+    parser = None
+    args = None
+
+    def __init__(self, *args, **kwargs):
+        self.parser = argparse.ArgumentParser()
+
+    def process(self):
+        self.args = self.parser.parse_args()
+
+
+if __name__ == "__main__":
+    twilist = Twilist()
+    twilist.process()
