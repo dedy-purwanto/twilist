ae1ab205989a357ad89fef33f1008ab3d6218461
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-14 17:48:52 +0800

    Add add_subparser functionality

--- twilist.py
+++ twilist.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 class Twilist():
 
     parser = None
-    args = None
+    subparsers = None
+    filepath = 'twilist'
+    file = None
+    text_max_length = 150
 
     def __init__(self, *args, **kwargs):
         parser = argparse.ArgumentParser()
@@ -14,18 +18,42 @@ class Twilist():
 
         add_subparser = subparsers.add_parser('add')
         add_subparser.add_argument('text')
+        add_subparser.add_argument('action', nargs='?', default='draft',
+                help="'draft' (default) or 'send'")
+        add_subparser.set_defaults(func=self.add_subparser)
 
         send_subparser = subparsers.add_parser('send')
-        send_subparser.add_argument('target', nargs='?',
-                help='first, last, rand, or index from list')
+        send_subparser.add_argument('target', nargs='?', 
+                default='first',
+                help="'first' (default), 'last', 'rand'"\
+                     ", or index from list")
 
         subparsers.add_parser('list')
 
         self.parser = parser
+        self.subparsers = subparsers
 
+        self.file = open(self.filepath, 'a')
+
+    def add_subparser(self, namespace):
+        if len(namespace.text) > self.text_max_length:
+            print "Text longer than %s characters!" % self.text_max_length
+            return
+
+        if namespace.action == 'draft':
+            self.file.write("%s%s" % (namespace.text, os.linesep))
+            print 'Saved as draft.'
+
+        if namespace.action == 'send':
+            print 'Sending tweet without saving..'
+            self.send_tweet(namespace.text)
+
+    def send_tweet(self, text):
+        print 'Sent'
 
     def process(self):
-        self.args = self.parser.parse_args()
+        cmd = self.parser.parse_args()
+        cmd.func(cmd)
 
 
 if __name__ == "__main__":
