c3de710ad7c98967c8dc5e56853fd8e24fe29c44
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-16 21:04:44 +0800

    Change quote chars

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='twilist',
-    packages=['twilist'],
-    version=['0.1'],
-    description=['managing tweet draft through CLI'],
-    author='kecebongsoft',
-    author_email='kecebongsoft@gmail.com',
-    url='http://github.com/kecebongsoft/twilist',
-    keywords=['tweet','twidge'],
+    name="twilist",
+    packages=["twilist"],
+    version=["0.1"],
+    description=["managing tweet draft through CLI"],
+    author="kecebongsoft",
+    author_email="kecebongsoft@gmail.com",
+    url="http://github.com/kecebongsoft/twilist",
+    keywords=["tweet","twidge"],
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Development Status :: 0.1 - Beta',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 0.1 - Beta",
     ],
     long_description="""\
     twilist
