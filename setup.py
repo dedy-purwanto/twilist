6ef3ed86d967291985f73f5f2ae1f24f543487ca
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-16 20:57:13 +0800

    Added basic setup information

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+description = """
+    twilist is a CLI tool for you to manage your tweet drafts from
+    command line, for full usage, see readme.md on the github repo:
+
+    http://github.com/kecebongsoft/twilist
+"""
+
+setup(
+    name='twilist',
+    packages=['twilist'],
+    version=['0.1'],
+    description=['managing tweet draft through CLI'],
+    author='kecebongsoft',
+    author_email='kecebongsoft@gmail.com',
+    url='http://github.com/kecebongsoft/twilist',
+    keywords=['tweet','twidge'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 0.1 - Beta',
+    ],
+    long_description=description
+)
